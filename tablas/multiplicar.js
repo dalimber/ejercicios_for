16709a031a2e03608a0b2db945f9e6ac56044716
dalimberAndrade <dalimberpubg@gmail.com>
2026-07-13 21:06:12 -0500

    Mejorar funcion generarTabla que automatiza tabla y cambia tiutlo

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -1,5 +1,17 @@
 function generarTablas() 
 {
+    let contenido="";
+    let tabla=3;
     let cmpContenedor=document.getElementById("contenedor");
-    cmpContenedor.innerHTML="<h1>PROBANDO</h1>";
+    let cmpTitulo=document.getElementById("txtTitiulo");
+    cmpTitulo.innerText="🌈 Aprende la Tabla del "+tabla+" 🌈";
+    //cmpContenedor.innerHTML="<h1>PROBANDO</h1>";
+    for (let index = 1; index <=12; index++) 
+        {
+            let resultado=tabla*index;
+            contenido=contenido+"<div class='fila' id='items'><span>3 × "+index
+        +"</span><span>=</span><strong>"+resultado+"</strong></div>";
+        cmpContenedor.innerHTML=contenido;
+        
+    }
 }
